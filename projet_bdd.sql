187f9d36287731148d838861ae8e131e2cdc0510
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2020-05-02 18:50:01 +0200

    update bdd

--- projet_bdd.sql
+++ projet_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  jeu. 16 avr. 2020 à 15:19
+-- Généré le :  sam. 02 mai 2020 à 18:49
 -- Version du serveur :  10.4.6-MariaDB
 -- Version de PHP :  7.3.9
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `projet_bdd`
+-- Base de données :  `psychologue`
 --
 
 -- --------------------------------------------------------
@@ -33,6 +33,13 @@ CREATE TABLE `admin` (
   `Email` varchar(255) NOT NULL,
   `Mdp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`Id_admin`, `Email`, `Mdp`) VALUES
+(1, 'admin@admin.admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -58,17 +65,20 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`Id_Client`, `Nom`, `Prenom`, `Genre`, `Email`, `Mdp`, `Age`, `Situation`, `Couple_avec`, `Moyen_connu`) VALUES
-(4, 'nom', 'prenom', 'femme', '', '', 45, 'non', 0, '');
+(4, 'nom', 'prenom', 'femme', '', '', 45, 'non', 0, ''),
+(5, 'test', 'test', 'femme', 'test@test', '', 5, 'non', 0, 'oui'),
+(6, 'Jack', 'Arta', 'homme', 'jackarta@test.test', 'oui', 38, 'non', 0, ''),
+(7, 'Paul', 'Ogne', 'enfant', 'paulogne@test.test', '', 5, 'non', 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `client_seance`
+-- Structure de la table `profession`
 --
 
-CREATE TABLE `client_seance` (
-  `Id_client` int(11) NOT NULL,
-  `Id_seance` int(11) NOT NULL
+CREATE TABLE `profession` (
+  `Id_profession` int(11) NOT NULL,
+  `descrition` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -78,22 +88,9 @@ CREATE TABLE `client_seance` (
 --
 
 CREATE TABLE `profession_client` (
-  `Id_profession` varchar(255) NOT NULL,
+  `Id_profession` int(11) NOT NULL,
   `Id_client` int(11) NOT NULL,
   `Date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `psy`
---
-
-CREATE TABLE `psy` (
-  `Id_psy` int(11) NOT NULL,
-  `Nom` varchar(255) NOT NULL,
-  `Prenom` varchar(255) NOT NULL,
-  `Mdp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -104,15 +101,25 @@ CREATE TABLE `psy` (
 
 CREATE TABLE `seance` (
   `Id_seance` int(11) NOT NULL,
-  `Id_psy` int(11) NOT NULL,
   `Date` date NOT NULL,
   `Heure` time NOT NULL,
-  `Prix` int(11) NOT NULL,
-  `Moyen_paiement` varchar(255) NOT NULL,
-  `Remarques` text NOT NULL,
+  `Prix` int(11) DEFAULT NULL,
+  `Moyen_paiement` varchar(255) DEFAULT NULL,
+  `Remarques` text DEFAULT NULL,
   `Nombre_client` int(11) NOT NULL,
-  `Note_anxiete` int(11) NOT NULL
+  `Note_anxiete` int(11) DEFAULT NULL,
+  `Id_client1` int(11) NOT NULL,
+  `Id_client2` int(11) DEFAULT NULL,
+  `Id_client3` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `seance`
+--
+
+INSERT INTO `seance` (`Id_seance`, `Date`, `Heure`, `Prix`, `Moyen_paiement`, `Remarques`, `Nombre_client`, `Note_anxiete`, `Id_client1`, `Id_client2`, `Id_client3`) VALUES
+(46, '2020-05-11', '11:30:00', NULL, NULL, NULL, 3, NULL, 4, 6, 7),
+(47, '2020-05-11', '11:00:00', NULL, NULL, NULL, 3, NULL, 4, 6, 7);
 
 --
 -- Index pour les tables déchargées
@@ -132,11 +139,10 @@ ALTER TABLE `client`
   ADD KEY `Client_Client` (`Couple_avec`);
 
 --
--- Index pour la table `client_seance`
+-- Index pour la table `profession`
 --
-ALTER TABLE `client_seance`
-  ADD PRIMARY KEY (`Id_client`,`Id_seance`),
-  ADD KEY `Seance_Client_Seance` (`Id_seance`);
+ALTER TABLE `profession`
+  ADD PRIMARY KEY (`Id_profession`);
 
 --
 -- Index pour la table `profession_client`
@@ -146,17 +152,13 @@ ALTER TABLE `profession_client`
   ADD KEY `Client_Profession_Client` (`Id_client`);
 
 --
--- Index pour la table `psy`
---
-ALTER TABLE `psy`
-  ADD PRIMARY KEY (`Id_psy`);
-
---
 -- Index pour la table `seance`
 --
 ALTER TABLE `seance`
   ADD PRIMARY KEY (`Id_seance`),
-  ADD KEY `Spy_Seance` (`Id_psy`);
+  ADD KEY `Seance_client1` (`Id_client1`),
+  ADD KEY `Seance_client2` (`Id_client2`),
+  ADD KEY `Seance_client3` (`Id_client3`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -166,48 +168,44 @@ ALTER TABLE `seance`
 -- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `Id_admin` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `Id_Client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id_Client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT pour la table `psy`
+-- AUTO_INCREMENT pour la table `profession`
 --
-ALTER TABLE `psy`
-  MODIFY `Id_psy` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `profession`
+  MODIFY `Id_profession` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `seance`
 --
 ALTER TABLE `seance`
-  MODIFY `Id_seance` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_seance` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `client_seance`
---
-ALTER TABLE `client_seance`
-  ADD CONSTRAINT `Client_Client_Seance` FOREIGN KEY (`Id_client`) REFERENCES `client` (`Id_Client`),
-  ADD CONSTRAINT `Seance_Client_Seance` FOREIGN KEY (`Id_seance`) REFERENCES `seance` (`Id_seance`);
-
---
 -- Contraintes pour la table `profession_client`
 --
 ALTER TABLE `profession_client`
-  ADD CONSTRAINT `Client_Profession_Client` FOREIGN KEY (`Id_client`) REFERENCES `client` (`Id_Client`);
+  ADD CONSTRAINT `Client_Profession_Client` FOREIGN KEY (`Id_client`) REFERENCES `client` (`Id_Client`),
+  ADD CONSTRAINT `Client_Profession_Profession` FOREIGN KEY (`Id_profession`) REFERENCES `profession` (`Id_profession`);
 
 --
 -- Contraintes pour la table `seance`
 --
 ALTER TABLE `seance`
-  ADD CONSTRAINT `Spy_Seance` FOREIGN KEY (`Id_psy`) REFERENCES `psy` (`Id_psy`);
+  ADD CONSTRAINT `Seance_client1` FOREIGN KEY (`Id_client1`) REFERENCES `client` (`Id_Client`),
+  ADD CONSTRAINT `Seance_client2` FOREIGN KEY (`Id_client2`) REFERENCES `client` (`Id_Client`),
+  ADD CONSTRAINT `Seance_client3` FOREIGN KEY (`Id_client3`) REFERENCES `client` (`Id_Client`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
