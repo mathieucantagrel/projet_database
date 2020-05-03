59f61b40ef0a54af23187348f7779cabcdb17b82
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2020-05-03 12:15:17 +0200

    ajout fonctionnalité calendrier

--- html/calendrier.php
+++ html/calendrier.php
@@ -1,8 +1,21 @@
 <?php
 include '../php/affichage_calendrier_psy.php';
 $dernier_lundi = $_GET['date'];
+
 $prochain_lundi = date('Y-m-d', strtotime($dernier_lundi. ' + 1 weeks'));
 $precedent_lundi = date('Y-m-d', strtotime($dernier_lundi. ' - 1 weeks'));
+
+$prochain_mois = date('Y-m-d', strtotime($dernier_lundi. ' + 1 months'));
+$prochain_mois = date("Y-m-d", strtotime("previous monday", strtotime($prochain_mois)));
+
+$precedent_mois = date('Y-m-d', strtotime($dernier_lundi. ' - 1 months'));
+$precedent_mois = date("Y-m-d", strtotime("previous monday", strtotime($precedent_mois)));
+
+$prochaine_annee = date('Y-m-d', strtotime($dernier_lundi. ' + 1 years'));
+$prochaine_annee = date("Y-m-d", strtotime("previous monday", strtotime($prochaine_annee)));
+
+$precedente_annee = date('Y-m-d', strtotime($dernier_lundi. ' - 1 years'));
+$precedente_annee = date("Y-m-d", strtotime("previous monday", strtotime($precedente_annee)));
 ?>
 
 <!doctype html>
@@ -20,6 +33,13 @@ $precedent_lundi = date('Y-m-d', strtotime($dernier_lundi. ' - 1 weeks'));
         affichage($dernier_lundi);
     ?>
 </table>
+<p>semaine:</p>
+<a href="<?php echo "calendrier.php?date=".$precedent_lundi?>"><input type="submit" value="<-"></a>
+<a href="<?php echo "calendrier.php?date=".$prochain_lundi?>"><input type="submit" value="->"></a>
+<p>mois:</p>
+<a href="<?php echo "calendrier.php?date=".$precedent_mois?>"><input type="submit" value="<-"></a>
+<a href="<?php echo "calendrier.php?date=".$prochain_mois?>"><input type="submit" value="->"></a>
+<p>annee:</p>
 <a href="<?php echo "calendrier.php?date=".$precedent_lundi?>"><input type="submit" value="<-"></a>
 <a href="<?php echo "calendrier.php?date=".$prochain_lundi?>"><input type="submit" value="->"></a>
 </body>
