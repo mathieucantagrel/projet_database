f621809f32b1842e113b6eebadffbfc876eba7e1
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2020-05-03 11:53:06 +0200

    edition rdv

--- html/Edition_rdv.php
+++ html/Edition_rdv.php
@@ -4,6 +4,29 @@ $id_seance = $_GET['id'];
 $sql = "SELECT * FROM `seance` WHERE `Id_seance`= $id_seance LIMIT 1";
 $result = mysqli_query($conn, $sql);
 $rows = mysqli_fetch_array($result);
+$datetime = date("Y-m-d");
+$h = date('H', strtotime($rows['Heure']));
+$min = date('i', strtotime($rows['Heure']));
+$id_client1 = isset($rows['Id_client1']) ? $rows['Id_client1'] : "";
+$id_client2 = isset($rows['Id_client2']) ? $rows['Id_client2'] : NULL;
+$id_client3 = isset($rows['Id_client3']) ? $rows['Id_client3'] : NULL;
+
+$sql = "SELECT Nom, Prenom From `client` WHERE Id_client = $id_client1 LIMIT 1";
+$result_client1 = mysqli_query($conn, $sql);
+$donnees_client1 = mysqli_fetch_array($result_client1);
+
+if ($id_client2!=NULL){
+    $sql = "SELECT Nom, Prenom From `client` WHERE Id_client = $id_client2 LIMIT 1";
+    $result_client2 = mysqli_query($conn, $sql);
+    $donnees_client2 = mysqli_fetch_array($result_client2);
+
+    if ($id_client3!=NULL){
+        $sql = "SELECT Nom, Prenom From `client` WHERE Id_client = $id_client3 LIMIT 1";
+        $result_client3 = mysqli_query($conn, $sql);
+        $donnees_client3 = mysqli_fetch_array($result_client3);
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -16,47 +39,100 @@ $rows = mysqli_fetch_array($result);
     <title>Document</title>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>
-                Date :
-            </td>
-            <td>
-                <?php echo $rows['Date']?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Heure :
-            </td>
-            <td>
-                <?php echo $rows['Heure']?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Prix :
-            </td>
-            <td>
-                <?php echo $rows['Prix']?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Moyen de paiement :
-            </td>
-            <td>
-                <?php echo $rows['Moyen_paiement']?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Remarques :
-            </td>
-            <td>
-                <?php echo $rows['Remarques']?>
-            </td>
-        </tr>
-    </table>
+    <form action="../php/edit_rdv_action.php?id=<?php echo $id_seance?>" method="post">
+        <table>
+            <tr>
+                <td>
+                    Date :
+                </td>
+                <td>
+                    <input type="date" value="<?php echo $rows['Date']?>" min="<?php echo $datetime ?>" name="date" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Heure :
+                </td>
+                <td>
+                    <input type="number" min="8" max="20" required name="heure" value="<?php echo $h?>">:
+                    <select required name="minute">
+                        <option value="00"<?php if ($min==00){echo "selected";}?>>00</option>
+                        <option value="30"<?php if ($min==30){echo "selected";}?>>30</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Prix :
+                </td>
+                <td>
+                    <input type="number" value="<?php echo $rows['Prix']?>" name="prix">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Moyen de paiement :
+                </td>
+                <td>
+                    <input type="text" value="<?php echo $rows['Moyen_paiement']?>" name="moyen_paiement">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Remarques :
+                </td>
+                <td>
+                    <textarea name="remarques"><?php echo $rows['Remarques']?></textarea>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Note d'anxiete:
+                </td>
+                <td>
+                    <input type="number" min="0" max="10" value="<?php echo $rows['Note_anxiete']?>" name="note_anxiete">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    client 1:
+                </td>
+                <td>
+                    <input type="text" value="<?php echo $donnees_client1['Nom']?>" name="nom1" required>
+                </td>
+                <td>
+                    <input type="text" value="<?php echo $donnees_client1['Prenom']?>" name="prenom1" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    client 2:
+                </td>
+                <td>
+                    <input type="text" value="<?php if ($id_client2!=NULL){echo $donnees_client2['Nom'];}?>" name="nom2">
+                </td>
+                <td>
+                    <input type="text" value="<?php if ($id_client2!=NULL){echo $donnees_client2['Prenom'];}?>" name="prenom2">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    client 3:
+                </td>
+                <td>
+                    <input type="text" value="<?php if ($id_client3!=NULL){echo $donnees_client3['Nom'];}?>" name="nom3">
+                </td>
+                <td>
+                    <input type="text" value="<?php if ($id_client3!=NULL){echo $donnees_client3['Prenom'];}?>" name="prenom3">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit">
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
---
+++ php/edit_rdv_action.php
@@ -0,0 +1,114 @@
+<?php
+
+include 'conn.php';
+
+$id_seance = $_GET['id'];
+
+$date = isset($_POST['date']) ? $_POST['date'] : NULL;
+$heure = isset($_POST['heure']) ? $_POST['heure'] : NULL;
+$minutes = isset($_POST['minute']) ? $_POST['minute'] : NULL;
+$prix = isset($_POST['prix']) ? $_POST['prix'] : NULL;
+$moyen_paiement = isset($_POST['moyen_paiement']) ? $_POST['moyen_paiement'] : NULL;
+$remarques = isset($_POST['remarques']) ? $_POST['remarques'] : NULL;
+$note_anxiete = isset($_POST['note_axiete']) ? $_POST['note_axiete'] : NULL;
+$nom1 = isset($_POST['nom1']) ? $_POST['nom1'] : NULL;
+$prenom1 = isset($_POST['prenom1']) ? $_POST['prenom1'] : NULL;
+$nom2 = isset($_POST['nom2']) ? $_POST['nom2'] : NULL;
+$prenom2 = isset($_POST['prenom2']) ? $_POST['prenom2'] : NULL;
+$nom3 = isset($_POST['nom3']) ? $_POST['nom3'] : NULL;
+$prenom3 = isset($_POST['prenom3']) ? $_POST['prenom3'] : NULL;
+
+$nbr_client = 1;
+$id_client2=NULL;
+$id_client3=NULL;
+
+$sql = "SELECT Id_client FROM `client` WHERE Nom='$nom1' AND Prenom='$prenom1'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+$id_client1 = $row['Id_client'];
+if ($id_client1==NULL){
+    die("<script>window.history.back()</script>");
+}
+
+echo $id_client1;
+
+if ($nom2!=NULL&&$prenom2!=NULL){
+    $sql = "SELECT Id_client FROM `client` WHERE Nom='$nom2' AND Prenom='$prenom2'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $id_client2 = $row['Id_client'];
+    if ($id_client2 == NULL) {
+        die("<script>window.history.back()</script>");
+    }
+    $nbr_client +=1;
+    echo $id_client2;
+}
+
+if ($nom3!=NULL&&$prenom3!=NULL) {
+    $sql = "SELECT Id_client FROM `client` WHERE Nom='$nom3' AND Prenom='$prenom3'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $id_client3 = $row['Id_client'];
+    if ($id_client3 == NULL) {
+        die("<script>window.history.back()</script>");
+    }
+    $nbr_client +=1;
+    echo $id_client3;
+}
+
+$heure_rdv = date('H:i:s', strtotime("$heure:$minutes:00"));
+
+$sql = "SELECT Id_seance FROM `seance` WHERE DATE='$date' AND Heure='$heure_rdv'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+if ($row['Id_seance']!=$id_seance&&$row['Id_seance']!=NULL){
+    die("<script>window.history.back()</script>");
+}
+
+if ($id_client2==NULL&&$id_client3==NULL){
+    $sql = "UPDATE `seance` 
+        SET `Date`='$date',
+        `Heure`='$heure_rdv',
+        `Prix`='$prix',
+        `Moyen_paiement`='$moyen_paiement',
+        `Remarques`='$remarques',
+        `Nombre_client`='$nbr_client',
+        `Note_anxiete`='$note_anxiete',
+        `Id_client1`='$id_client1',
+        `Id_client2`=NULL,
+        `Id_client3`=NULL
+        WHERE `Id_seance`='$id_seance'";
+}elseif ($id_client2!=NULL&&$id_client3==NULL){
+    $sql = "UPDATE `seance` 
+        SET `Date`='$date',
+        `Heure`='$heure_rdv',
+        `Prix`='$prix',
+        `Moyen_paiement`='$moyen_paiement',
+        `Remarques`='$remarques',
+        `Nombre_client`='$nbr_client',
+        `Note_anxiete`='$note_anxiete',
+        `Id_client1`='$id_client1',
+        `Id_client2`=$id_client2,
+        `Id_client3`=NULL
+        WHERE `Id_seance`='$id_seance'";
+}else{
+    $sql = "UPDATE `seance` 
+        SET `Date`='$date',
+        `Heure`='$heure_rdv',
+        `Prix`='$prix',
+        `Moyen_paiement`='$moyen_paiement',
+        `Remarques`='$remarques',
+        `Nombre_client`='$nbr_client',
+        `Note_anxiete`='$note_anxiete',
+        `Id_client1`='$id_client1',
+        `Id_client2`='$id_client2',
+        `Id_client3`='$id_client3'
+        WHERE `Id_seance`='$id_seance'";
+}
+
+if (!mysqli_query($conn, $sql)){
+    echo mysqli_error($conn);
+}
+
+$dernier_lundi = date("Y-m-d", strtotime("previous monday"));
+header("location: ../html/calendrier.php?date=$dernier_lundi");
